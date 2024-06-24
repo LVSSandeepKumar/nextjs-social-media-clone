d2971c0adf8aae1807837844fbad148e06553a35
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-06-24 13:06:49 +0530

    Save User Info

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,29 @@
-"use client"
-import Image from 'next/image'
-import { useEffect } from 'react';
-import GlobalApi from './_utils/GlobalApi';
-import { useUser } from '@clerk/nextjs';
+"use client";
+import Image from "next/image";
+import { useEffect } from "react";
+import GlobalApi from "./_utils/GlobalApi";
+import { useUser } from "@clerk/nextjs";
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 
 export default function Home() {
-  
-  return (
-  <div>
-    Home
-  </div>
-  )
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && createUserProfile();
+  }, [user]);
+
+  const createUserProfile = () => {
+    if (!localStorage.getItem('isLogin')) {
+      const data = {
+        name: user.fullName,
+        email: user.primaryEmailAddress.emailAddress,
+        image: user.imageUrl,
+      };
+      GlobalApi.createUser(data).then((resp) => {
+        console.log(resp.data);
+        localStorage.setItem('isLogin', true);
+      });
+    }
+  };
+  return <div></div>;
 }
