33dbc6990908cb20cd9c830017b20108c0c6973f
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-06-24 11:37:34 +0530

    Authentication added with clerk

--- app/(routes)/home/_components/Banner.jsx
+++ app/(routes)/home/_components/Banner.jsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Banner() {
@@ -7,12 +8,14 @@ function Banner() {
     <div className='p-2 bg-white-400 
     rounded-xl shadow-sm flex items-center
     bg-blue-100
-     border gap-5'>
+     border gap-5'> 
         <Image src='/social-panda-1.png' width={200} height={200} alt='panda'/>
         <div>
         <h2 className='font-bold text-[29px] '>WELCOME TO SOCIAL PANDA</h2>
         <h2 className=''>Join Community, Create and Share your thought</h2>
-        <Button className="mt-3 bg-blue-500">Get Started</Button>
+        <Link href='/sign-up'>
+          <Button className="mt-3 bg-blue-500">Get Started</Button>
+        </Link>
         </div>
         
     </div>
